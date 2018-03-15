8387e5500d72294b0a038a874280b386a1397137
alexrevell <revellalex@gmail.com>
2018-03-15 23:04:34 +1000

    Add tests for api and validateCard

---
+++ test/api.js
@@ -0,0 +1,64 @@
+'use strict'
+
+// requires for testing
+const Code        = require('code')
+const expect      = Code.expect
+const Lab         = require('lab')
+const lab         = exports.lab = Lab.script()
+
+// use some BDD verbage instead of lab default
+const describe    = lab.describe
+const it          = lab.it
+const after       = lab.after
+
+// require hapi server
+const Server = require('../server.js')
+
+// tests
+describe('testing API', () => {
+
+  it('gets index route', async () => {
+
+    const response = await Server.inject({
+      method: 'GET',
+      url: '/'
+    })
+
+    expect(response.statusCode).to.equal(200)
+  })
+
+  it('posts to index route with valid card number', async () => {
+    const validNumber = '4111111111111111'
+
+    const response = await Server.inject({
+      method: 'POST',
+      url: '/',
+      payload: { card: validNumber }
+    })
+
+    expect(response.statusCode).to.equal(200)
+    expect(response.result).to.match(/Card number is valid/)
+  })
+  it('posts to index route with invalid card number', async () => {
+    const invalidNumber = '1hja55'
+
+    const response = await Server.inject({
+      method: 'POST',
+      url: '/',
+      payload: { card: invalidNumber }
+    })
+
+    expect(response.statusCode).to.equal(200)
+    expect(response.result).to.match(/Card number is invalid/)
+  })
+
+  it('should return error for invalid id', async () => {
+
+    const response = await Server.inject({
+      method: 'GET',
+      url: '/api/products/5'
+    })
+
+    expect(response.statusCode).to.equal(404)
+  })
+})
---
+++ test/validateCard.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// requires for testing
+const Code        = require('code')
+const expect      = Code.expect
+const Lab         = require('lab')
+const lab         = exports.lab = Lab.script()
+
+// use some BDD verbage instead of lab default
+const describe    = lab.describe
+const it          = lab.it
+const after       = lab.after
+
+// require hapi server
+const validateCard = require('../validateCard.js')
+
+// tests
+describe('testing validateCard', () => {
+
+  it('correctly validates a valid card number', async () => {
+    const isValid = await validateCard('4111111111111111')
+
+    expect(isValid).to.be.true
+  })
+  it('correctly validates an invalid card number', async () => {
+    const isValid = await validateCard('4jh5ie')
+
+    expect(isValid).to.be.false
+  })
+})
